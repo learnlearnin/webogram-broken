ddbca584fe8d1aac494cb35e7f7d2b6e0d07b8c8
Akshay S Dinesh <asdofindia@gmail.com>
2014-10-14 02:18:06 +0530

    testi

--- js/username.js
+++ js/username.js
@@ -1,7 +1,7 @@
 $("#submit").on('click',function(){
 	alert($("#username").val());
-	a=angular.injector(['myApp']);
-	b=a.get('MtpApiManager');
+	//a=angular.injector(['myApp']);
+	//b=a.get('MtpApiManager');
 	//b.invokeApi('account.checkUsername',{username:});
-	b.invokeApi('account.updateUsername',{username:$("#username".val())});
+	//b.invokeApi('account.updateUsername',{username:$("#username".val())});
 });
